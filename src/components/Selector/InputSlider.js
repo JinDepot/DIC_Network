addf447ed8e2eeadbd76f4c78b954245c4431a24
Youngseok Hahm <terryhahm@gmail.com>
2022-05-02 01:12:36 +0900

    Added Tooltip and Description

--- src/components/Selector/InputSlider.js
+++ src/components/Selector/InputSlider.js
@@ -1,79 +1,152 @@
-import React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import Slider from '@mui/material/Slider';
-import MuiInput from '@mui/material/Input';
-
+import React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Slider from "@mui/material/Slider";
+import MuiInput from "@mui/material/Input";
 
 const Input = styled(MuiInput)`
-  width: 42px;
+    width: 42px;
 `;
 
 export default function InputSlider({ nodes, config, setConfig }) {
+    const handleSliderChange = (event, newValue) => {
+        setConfig({
+            ...config,
+            n: newValue,
+        });
+    };
 
-	const handleSliderChange = (event, newValue) => {
-		setConfig({
-			...config,
-			n : newValue
-		});
-	};
+    const handleInputChange = (event) => {
+        setConfig({
+            ...config,
+            n: event.target.value === "" ? "" : Number(event.target.value),
+        });
+    };
 
-	const handleInputChange = (event) => {
-		setConfig({
-			...config,
-			n : event.target.value === '' ? '' : Number(event.target.value)
-		})
-	};
+    const handleBlur = () => {
+        if (config.n < 1) {
+            setConfig({
+                ...config,
+                n: 1,
+            });
+        } else if (config.n > nodes.length) {
+            setConfig({
+                ...config,
+                n: nodes.length,
+            });
+        }
+    };
 
-	const handleBlur = () => {
-		if (config.n < 1) {
-			setConfig({
-				...config,
-				n: 1
-			})
-		} else if (config.n > nodes.length) {
-			setConfig({
-				...config,
-				n: nodes.length
-			})
-		}
-	};
+    return (
+        <div style={{ width: "inherit" }}>
+            <a id="input-slider" className={`optionHeader noselect`}>
+                Top N Node (Marketcap)
+            </a>
+            <Grid container spacing={2} alignItems="center">
+                <Grid item xs className={`inputSlider`}>
+                    <Slider
+                        min={1}
+                        max={nodes.length}
+                        value={typeof config.n === "number" ? config.n : 1}
+                        onChange={handleSliderChange}
+                        aria-labelledby="input-slider"
+                    />
+                    <div
+                        className={`inputSlider_tooltip`}
+                        style={{
+                            padding: "2px 8px",
+                        }}
+                    >
+                        <span className={`tooltipText`}>
+                            Choose the number of nodes N (top-N by market cap
+                            size) to be included in the network
+                        </span>
+                    </div>
+                </Grid>
+                <Grid item className={`inputSlider`}>
+                    <Input
+                        style={{ width: "60px", alignItems: "center" }}
+                        className={""}
+                        value={config.n}
+                        margin="dense"
+                        onChange={handleInputChange}
+                        onBlur={handleBlur}
+                        inputProps={{
+                            step: 10,
+                            min: 1,
+                            max: nodes.length,
+                            type: "number",
+                            "aria-labelledby": "input-slider",
+                        }}
+                    />
+                    <div
+                        className={`inputSlider_tooltip`}
+                        style={{
+                            left: "230%",
+                            padding: "2px 8px",
+                        }}
+                    >
+                        <span className={`tooltipText`}>
+                            Type the number of nodes N (top-N by market cap
+                            size) to be included in the network
+                        </span>
+                    </div>
+                </Grid>
+            </Grid>
+        </div>
+    );
+}
 
-	return (
-		<div style={{ width: 'inherit' }}>
-			<a id="input-slider" className={`optionHeader noselect`}>
-				Top N Node (Marketcap)
-			</a>
-			<Grid container spacing={2} alignItems="center">
-				<Grid item xs>
-					<Slider
-						min={1}
-						max={nodes.length}
-						value={typeof config.n === 'number' ? config.n : 1}
-						onChange={handleSliderChange}
-						aria-labelledby="input-slider"
-					/>
-				</Grid>
-				<Grid item>
-					<Input
-						style={{width: '60px', alignItems: 'center'}}
-						className={""}
-						value={config.n}
-						margin="dense"
-						onChange={handleInputChange}
-						onBlur={handleBlur}
-						inputProps={{
-							step: 10,
-							min: 1,
-							max: nodes.length,
-							type: 'number',
-							'aria-labelledby': 'input-slider',
-						}}
-					/>
-				</Grid>
-			</Grid>
+{
+    /* <div style={{ width: "inherit" }}>
+<a id="input-slider" className={`optionHeader noselect`}>
+	Top N Node (Marketcap)
+</a>
+<Grid container spacing={2} alignItems="center">
+	<div className={`tooltip`} style={{ width: "90%" }}>
+		<Grid item xs>
+			<Slider
+				min={1}
+				max={nodes.length}
+				value={typeof config.n === "number" ? config.n : 1}
+				onChange={handleSliderChange}
+				aria-labelledby="input-slider"
+			/>
+		</Grid>
+		<div className={`tooltip_text`}>
+			<span>
+				Choose "S&P500" for the U.S. market; "KOSPI+KOSDAQ"
+				for Korean market
+			</span>
 		</div>
-	);
+	</div>
+	<div className={`tooltip`} style={{ width: "10%" }}>
+		<Grid item>
+			<Input
+				style={{ width: "60px", alignItems: "center" }}
+				className={""}
+				value={config.n}
+				margin="dense"
+				onChange={handleInputChange}
+				onBlur={handleBlur}
+				inputProps={{
+					step: 10,
+					min: 1,
+					max: nodes.length,
+					type: "number",
+					"aria-labelledby": "input-slider",
+				}}
+			/>
+		</Grid>
+		<div className={`tooltip_text`}>
+			<span>
+				Choose "S&P500" for the U.S. market; "KOSPI+KOSDAQ"
+				for Korean market
+			</span>
+		</div>
+	</div>
+</Grid>
+</div> */
 }
--- src/components/Selector/Selector.js
+++ src/components/Selector/Selector.js
@@ -1,36 +1,45 @@
-import React, { useState } from 'react';
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
-function Selector({title, config, setConfig}) {
-
+function Selector({ title, config, setConfig }) {
     const handleChange = (e) => {
         setConfig({
             ...config,
-            market: e.target.value
+            market: e.target.value,
         });
     };
 
     return (
-        <FormControl fullWidth>
-            <a id="demo-simple-select-label" className={`optionHeader noselect`}>
-				{title}
-			</a>
-            <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={config.market}
-                onChange={handleChange}
-            >
-                <MenuItem value="S&P500"> S&P 500 </MenuItem>
-                <MenuItem value="KOSPI"> KOSPI + KOSDAQ </MenuItem>
-            </Select>
-        </FormControl>
-
-    )
+        <div className={`tooltip`} style={{ width: "100%" }}>
+            <FormControl fullWidth>
+                <a
+                    id="demo-simple-select-label"
+                    className={`optionHeader noselect`}
+                >
+                    {title}
+                </a>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={config.market}
+                    onChange={handleChange}
+                >
+                    <MenuItem value="S&P500"> S&P 500 </MenuItem>
+                    <MenuItem value="KOSPI"> KOSPI + KOSDAQ </MenuItem>
+                </Select>
+            </FormControl>
+            <div className={`tooltip_text`}>
+                <span className={`tooltipText`}>
+                    Choose "S&P500" for the U.S. market <br></br> "KOSPI+KOSDAQ"
+                    for Korean market
+                </span>
+            </div>
+        </div>
+    );
 }
 
 export default Selector;
